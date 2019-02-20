a012b7619d5d1538675c4c3c9ef765eb75f8c82a
Por Suphunnee Prajongjai <37324049+PorSuphunnee@users.noreply.github.com>
2019-02-20 23:33:25 +0700

    Add files via upload

--- examples/offline_voice_assistant.py
+++ examples/offline_voice_assistant.py
@@ -26,8 +26,8 @@ def task(quit_event):
     mic = Microphone(quit_event=quit_event)
 
     while not quit_event.is_set():
-        if mic.wakeup('satja'):
-            print('Wake up por')
+        if mic.wakeup('respeaker'):
+            print('Wake up')
             data = mic.listen()
             text = mic.recognize(data)
             if text:
